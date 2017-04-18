21ce19cf634dbe0c2c37a2826c33846506c7f528
breedish <breedish@gmail.com>
2017-04-18 12:11:14 +0300

    Init repo

---
+++ ses-player-api/src/main/java/com/breedish/ses/ZssPlayerApiApplication.java
@@ -0,0 +1,12 @@
+package com.breedish.ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZssPlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZssPlayerApiApplication.class, args);
+	}
+}
---
+++ ses-player-service/src/test/java/com/breedish/ses/ZssPlayerServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.breedish.ses;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZssPlayerServiceApplicationTests
+{
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
