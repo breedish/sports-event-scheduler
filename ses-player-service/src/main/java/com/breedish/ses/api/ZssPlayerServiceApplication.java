c897460030eace67ddd8784a4fa5e858445f46b2
breedish <breedish@gmail.com>
2017-04-18 12:26:56 +0300

    Set to use testng

--- ses-player-service/src/main/java/com/breedish/ses/api/ZssPlayerServiceApplication.java
+++ ses-player-service/src/main/java/com/breedish/ses/api/ZssPlayerServiceApplication.java
@@ -1,4 +1,4 @@
-package com.breedish.ses;
+package com.breedish.ses.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ses-player-service/src/test/java/com/breedish/ses/api/ZssPlayerServiceApplicationTests.java
+++ ses-player-service/src/test/java/com/breedish/ses/api/ZssPlayerServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.breedish.ses;
+package com.breedish.ses.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
